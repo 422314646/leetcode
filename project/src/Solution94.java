364002b37b719fd0b8f79385e46193200b6c7276
w13609473683 <422314646@qq.com>
2020-08-16 10:41:54 +0800

    leetcode94

--- project/src/Solution94.java
+++ project/src/Solution94.java
@@ -1,5 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /*
-*
+*94. 二叉树的中序遍历
 *  */
 public class Solution94 {
+    List<Integer> res = new ArrayList<>();
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        preOder(root, res);
+        return res;
+    }
+
+    public void preOder(TreeNode root, List<Integer> res){
+        if (root == null){
+            return;
+        } else {
+            preOder(root.left, res);
+            res.add(root.val);
+            preOder(root.right, res);
+        }
+    }
+}
+
+class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode(int x) { val = x; }
 }
